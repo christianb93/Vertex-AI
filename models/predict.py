31c6b04b881fa90e06f1c19f653c285776c1d0b7
christianb93 <me@somewhere>
2024-01-21 09:39:25 +0100

    Add switch to select raw prediction

--- models/predict.py
+++ models/predict.py
@@ -1,4 +1,5 @@
 import os 
+import argparse
 import google.cloud.aiplatform as aip
 
 #
@@ -8,7 +9,15 @@ google_project_id = os.environ.get("GOOGLE_PROJECT_ID")
 google_region = os.environ.get("GOOGLE_REGION")
 aip.init(project = google_project_id, location = google_region)
 
-
+#
+# Get arguments
+#
+parser = argparse.ArgumentParser()
+parser.add_argument("--raw", 
+                    action = "store_true",
+                    default = False,
+                    help = "Use raw predictions")
+args = parser.parse_args()
 #
 # Get endpoint
 #
@@ -32,19 +41,18 @@ print(f"Using endpoint with ID {endpoint.name} ({endpoint.resource_name})")
 prediction = endpoint.predict(
     instances = [[0.5, 0.35]]
 )
-print("Making prediction:")
-print(f"Prediction: {prediction.predictions}")
-print(f"ModeL:      {prediction.model_resource_name}")
-print(f"Version:    {prediction.model_version_id}")
-#
-# Do the same using raw prediction
-#
-print("Making raw prediction:")
-raw_prediction = endpoint.raw_predict(
-    body = b'{"instances" : [[0.5, 0.35]]}',
-    headers = {"Content-Type": "application/json"},
-)
-print(f"Status:     {raw_prediction.status_code}")
-print(f"Prediction: {raw_prediction.json()}")
-print(f"Model:      {raw_prediction.headers['X-Vertex-AI-Model']}")
-print(f"Version:    {raw_prediction.headers['X-Vertex-AI-Model-Version-Id']}")
+if not args.raw:
+    print("Making prediction:")
+    print(f"Prediction: {prediction.predictions}")
+    print(f"ModeL:      {prediction.model_resource_name}")
+    print(f"Version:    {prediction.model_version_id}")
+else:
+    print("Making raw prediction:")
+    raw_prediction = endpoint.raw_predict(
+        body = b'{"instances" : [[0.5, 0.35]]}',
+        headers = {"Content-Type": "application/json"},
+    )
+    print(f"Status:     {raw_prediction.status_code}")
+    print(f"Prediction: {raw_prediction.json()['predictions']}")
+    print(f"Model:      {raw_prediction.headers['X-Vertex-AI-Model']}")
+    print(f"Version:    {raw_prediction.headers['X-Vertex-AI-Model-Version-Id']}")

7963c653ad6fe374147474c39c86fbff74cdf871
christianb93 <me@somewhere>
2024-01-27 14:29:40 +0100

    Submit pipeline

---
+++ pipelines/submit_pipeline.py
@@ -0,0 +1,31 @@
+import google.cloud.aiplatform as aip 
+import os
+
+google_project_id = os.environ.get("GOOGLE_PROJECT_ID")
+google_region = os.environ.get("GOOGLE_REGION")
+
+aip.init(project = google_project_id,
+         location = google_region)
+
+#
+# Create a pipeline job from the 
+# pipeline definition
+#
+pipeline_job = aip.PipelineJob(
+    display_name = "my-pipeline",
+    template_path = "my-pipeline.yaml",
+    pipeline_root = f"gs://vertex-ai-{google_project_id}/pipeline_root",
+    parameter_values = {
+        "epochs" : 100
+    },
+    project = google_project_id,
+    location = google_region
+)
+
+#
+# Submit the job
+#
+service_account = f"vertex-ai-run@{google_project_id}.iam.gserviceaccount.com"
+pipeline_job.submit(
+    service_account = service_account
+)

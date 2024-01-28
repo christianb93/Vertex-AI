6e17240abe006c81b7b1e8d1abbac9090af19389
christianb93 <me@somewhere>
2024-01-28 21:21:03 +0100

    Add tb_utils.py

--- docker/pipeline/build.sh
+++ docker/pipeline/build.sh
@@ -4,10 +4,12 @@
 # Create Dockerfile
 #
 cp ../../pipelines/model.py .
+cp ../../pipelines/tb_utils.py .
 cat  > Dockerfile <<EOF
 FROM $GOOGLE_REGION-docker.pkg.dev/$GOOGLE_PROJECT_ID/vertex-ai-docker-repo/base:latest
 
 COPY model.py .
+COPY tb_utils.py .
 
 ENTRYPOINT ["/bin/bash", "-c"]
 
